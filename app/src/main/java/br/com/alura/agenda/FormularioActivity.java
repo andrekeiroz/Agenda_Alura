2d53065abfa2743cfc7b3a9ea88986b59f4d30dc
Andre <andre.keiroz@hotmail.com>
2016-04-28 20:03:16 -0300

    Classe aluno e Formulario Helper

--- app/src/main/java/br/com/alura/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda/FormularioActivity.java
@@ -9,16 +9,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import br.com.alura.agenda.modelo.Aluno;
+
 public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-
-
-
+        helper = new FormularioHelper(this);
     }
 
     @Override
@@ -32,8 +34,13 @@ public class FormularioActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Toast.makeText(FormularioActivity.this, "Aluno salvo!", Toast.LENGTH_SHORT).show();
-        finish();
+       switch(item.getItemId()){
+           case R.id.menu_formulario_ok:
+               Aluno aluno = helper.pegaAluno();
+               Toast.makeText(FormularioActivity.this, "Aluno "+ aluno.getNome() +" salvo!", Toast.LENGTH_SHORT).show();
+               finish();
+               break;
+       }
         return super.onOptionsItemSelected(item);
 
     }
---
+++ app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
@@ -0,0 +1,7 @@
+package br.com.alura.agenda.dao;
+
+/**
+ * Created by Andre on 28-Apr-16.
+ */
+public class AlunoDAO {
+}
